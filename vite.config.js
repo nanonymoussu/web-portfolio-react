5b410f936273da5849a385c1be958fbc9952f909
nanonymoussu <nanon2546@yahoo.com>
2025-02-04 21:34:38 +0700

    Fix problems to publish to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	const base = env.GITHUB_ACTIONS ? '/web-portfolio-react/' : './';
+
 	return {
 		base,
 		server: {
